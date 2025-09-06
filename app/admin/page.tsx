6d463de8725e2f64b37a39e886699878fc711b88
tanvi <tanvipatel5176@gmail.com>
2025-09-06 18:12:56 +0530

    finished

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,40 +1,93 @@
-import { AppSidebar } from "@/components/sidebar/app-sidebar"
-import { ChartAreaInteractive } from "@/components/sidebar/chart-area-interactive"
-import { DataTable } from "@/components/sidebar/data-table"
-import { SectionCards } from "@/components/sidebar/section-cards"
-import { SiteHeader } from "@/components/sidebar/site-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
+import { ChartAreaInteractive } from "@/components/sidebar/chart-area-interactive";
+import { SectionCards } from "@/components/sidebar/section-cards";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { EmptyState } from "@/components/general/EmptyState";
+import { AdminCourseCard, AdminCourseCardSkeleton } from "./courses/_components/AdminCourseCard";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Suspense } from "react";
+import { adminGetRecentCourses } from "../data/admin/admin-get-recent-courses";
+import { adminGetEnrollmentStats } from "../data/admin/admin-get-enrollment-stats";
 
-import data from "./data.json"
 
-export default function AdminIndexPage() {
+export default async function AdminIndexPage() {
+
+  const enrollmentData = await adminGetEnrollmentStats();
+
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>
-              <DataTable data={data} />
-            </div>
-          </div>
+    <>
+      <SectionCards />
+
+      <ChartAreaInteractive data={enrollmentData} />    
+
+      <div className="space-y-4">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold">
+            Recent Courses
+          </h2>
+          <Link
+            className={buttonVariants({
+              variant: "outline",
+            })}
+            href="/admin/courses"
+          >
+            View All Courses
+          </Link>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+
+        <Suspense fallback={<RenderRecentCoursesSkeletonLayout />}>
+          <RenderRecentCourses />
+        </Suspense>
+      </div>
+    </>
   )
 }
+
+
+export const RenderRecentCourses = async() => {
+  
+  const data = await adminGetRecentCourses();
+  if(data.length === 0) {
+    return (
+      <EmptyState 
+        buttonText="Create a new Course"
+        description="You haven't created any courses yet. Create some to see them here."
+        title="You dont have any Courses yet!"
+        href="/admin/courses/create"
+      />
+    )
+  }
+
+  return (
+    <div className="relative px-12">
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        className="w-full"
+      >
+        <CarouselContent>
+          {data.map((course) => (
+            <CarouselItem key={course.id} className="md:basis-1/2">
+              <AdminCourseCard data={course} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  )
+}
+
+export const RenderRecentCoursesSkeletonLayout = async() => {
+  return (
+    <div className="relative px-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {Array.from({ length: 2 }).map((_, index) => (
+          <AdminCourseCardSkeleton key={index} />  
+        ))}
+      </div>
+    </div>
+)}
